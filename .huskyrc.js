33528d465228cd022bfb9df5e40667fb280be9bd
jamesjwood <jamesjwood@msn.com>
2021-08-12 10:28:01 +0100

    Add husky

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   hooks: {
-    'prepare-commit-msg': 'exec < /dev/tty && git cz --hook || true',
+    'prepare-commit-msg': 'exec < /dev/tty && npx cz --hook || true',
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
     'pre-commit': 'lint-staged',
     'pre-push': 'make',
