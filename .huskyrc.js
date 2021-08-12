290ddba83416475a6504c4936bea5451bfff37b3
jamesjwood <jamesjwood@msn.com>
2021-08-12 10:37:05 +0100

    build(husky): add husky config and lint-staged

--- .huskyrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  hooks: {
-    'prepare-commit-msg': 'exec < /dev/tty && npx cz --hook || true',
-    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
-    'pre-commit': 'lint-staged',
-    'pre-push': 'make',
-  },
-}
